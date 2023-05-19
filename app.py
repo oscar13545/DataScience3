822ac147f9f1c2daa913b34d22b2d61e137d396d
oscar13545 <55121243+oscar13545@users.noreply.github.com>
2023-05-18 23:37:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,20 @@ import joblib
 import tensorflow as tf
 import keras
 import numpy as np
-
+#lector del archivo txt
 with open("Michael_Jackson.txt") as f:
     shakespeare_text = f.read()
     
 "".join(sorted(set(shakespeare_text.lower())))
 
+#Generar los tokens con el archivo de texto
 tokenizer = keras.preprocessing.text.Tokenizer(char_level=True)
 tokenizer.fit_on_texts(shakespeare_text)
 
 tokenizer.texts_to_sequences(["First"])
 
 tokenizer.sequences_to_texts([[20, 6, 9, 8, 3]])
-max_id = len(tokenizer.word_index) # number of distinct characters
+max_id = len(tokenizer.word_index) 
 dataset_size = tokenizer.document_count 
 
 def preprocess(texts):
@@ -29,35 +30,30 @@ def next_char(text, temperature=1):
     rescaled_logits = tf.math.log(y_proba) / temperature
     char_id = tf.random.categorical(rescaled_logits, num_samples=1) + 1
     return tokenizer.sequences_to_texts(char_id.numpy())[0]
-
+# Es la funcion que genera la canción 
 def complete_song(seed_text, num_lines=4, chars_per_line=50, temperature=1):
     song = seed_text
 
     for _ in range(num_lines):
         for _ in range(chars_per_line):
             song += next_char(song, temperature)
-        song += '\n'  # Add a line break after each line
+        song += '\n'  
 
     return song
 
-# Load the model
+# Cargar el modelo
 model = joblib.load("MichaelJackson_model.pkl")
 
 def main():
-    st.title("Song Generator")
-
-    # Get user input for the starting word or phrase
-    seed_text = st.text_input("Enter the starting word or phrase")
-
+    st.title("Generador de letras de Michael Jackson")
+    seed_text = st.text_input("Introduzca la frase con la que quiere que empieza la letra")
     song = ""
+    
     if seed_text:
-        # Generate the song
         tf.random.set_seed(42)
         song = complete_song(seed_text, num_lines=8, chars_per_line=80, temperature=0.5)
-
-    # Display the generated song
-    st.subheader("Generated Song")
-    st.text_area("Song Lyrics", value=song, height=400)
+    st.subheader("Generador")
+    st.text_area("Letra", value=song, height=400)
 
 if __name__ == "__main__":
     main()
