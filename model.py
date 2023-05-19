4c9d952bd81585032d44ffe10792c68a5a251dad
oscar13545 <55121243+oscar13545@users.noreply.github.com>
2023-05-18 23:58:15 -0600

    Add files via upload

---
+++ model.py
@@ -0,0 +1,163 @@
+import time
+start_time = time.time()
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+# Python ≥3.5 is required
+import sys
+assert sys.version_info >= (3, 5)
+
+# Scikit-Learn ≥0.20 is required
+import sklearn
+assert sklearn.__version__ >= "0.20"
+
+# TensorFlow ≥2.0 is required
+import tensorflow as tf
+from tensorflow import keras
+assert tf.__version__ >= "2.0"
+
+if not tf.config.list_physical_devices('GPU'):
+    print("No GPU was detected. LSTMs and CNNs can be very slow without a GPU.")
+
+# Common imports
+import numpy as np
+import os
+import joblib
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+tf.random.set_seed(42)
+
+# To plot pretty figures
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "nlp"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+n_steps = 5
+dataset = tf.data.Dataset.from_tensor_slices(tf.range(15))
+dataset = dataset.window(n_steps, shift=2, drop_remainder=True)
+dataset = dataset.flat_map(lambda window: window.batch(n_steps))
+dataset = dataset.shuffle(10).map(lambda window: (window[:-1], window[1:]))
+dataset = dataset.batch(3).prefetch(1)
+
+shakespeare_url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+
+with open("Michael_Jackson.txt") as f:
+    shakespeare_text = f.read()
+
+
+
+"".join(sorted(set(shakespeare_text.lower())))
+
+tokenizer = keras.preprocessing.text.Tokenizer(char_level=True)
+tokenizer.fit_on_texts(shakespeare_text)
+
+tokenizer.texts_to_sequences(["First"])
+
+tokenizer.sequences_to_texts([[20, 6, 9, 8, 3]])
+max_id = len(tokenizer.word_index) # number of distinct characters
+dataset_size = tokenizer.document_count # total number of characters
+[encoded] = np.array(tokenizer.texts_to_sequences([shakespeare_text])) - 1
+train_size = dataset_size * 90 // 100
+dataset = tf.data.Dataset.from_tensor_slices(encoded[:train_size])
+
+
+n_steps = 100
+window_length = n_steps + 1 # target = input shifted 1 character ahead
+dataset = dataset.window(window_length, shift=1, drop_remainder=True)
+dataset = dataset.flat_map(lambda window: window.batch(window_length))
+np.random.seed(42)
+tf.random.set_seed(42)
+batch_size = 32
+dataset = dataset.shuffle(10000).batch(batch_size)
+dataset = dataset.map(lambda windows: (windows[:, :-1], windows[:, 1:]))
+dataset = dataset.map(
+    lambda X_batch, Y_batch: (tf.one_hot(X_batch, depth=max_id), Y_batch))
+dataset = dataset.prefetch(1)
+
+"""
+model = keras.models.Sequential([
+    keras.layers.GRU(128, return_sequences=True, input_shape=[None, max_id],
+                     #dropout=0.2, recurrent_dropout=0.2),
+                     dropout=0.2),
+    keras.layers.GRU(128, return_sequences=True,
+                     #dropout=0.2, recurrent_dropout=0.2),
+                     dropout=0.2),
+    keras.layers.TimeDistributed(keras.layers.Dense(max_id,
+                                                     activation="softmax"))
+])
+model.compile(loss="sparse_categorical_crossentropy", optimizer="adam")
+history = model.fit(dataset, epochs=10)
+
+joblib.dump(model, "MichaelJackson_model.pkl")
+"""
+model = joblib.load("MichaelJackson_model.pkl")
+
+def preprocess(texts):
+    X = np.array(tokenizer.texts_to_sequences(texts)) - 1
+    return tf.one_hot(X, max_id)
+
+X_new = preprocess(["How are yo"])
+#Y_pred = model.predict_classes(X_new)
+Y_pred = np.argmax(model(X_new), axis=-1)
+tokenizer.sequences_to_texts(Y_pred + 1)[0][-1] # 1st sentence, last char
+
+tf.random.set_seed(42)
+tf.random.categorical([[np.log(0.5), np.log(0.4), np.log(0.1)]], num_samples=40).numpy()
+
+def next_char(text, temperature=1):
+    X_new = preprocess([text])
+    y_proba = model(X_new)[0, -1:, :]
+    rescaled_logits = tf.math.log(y_proba) / temperature
+    char_id = tf.random.categorical(rescaled_logits, num_samples=1) + 1
+    return tokenizer.sequences_to_texts(char_id.numpy())[0]
+
+tf.random.set_seed(42)
+
+next_char("How are yo", temperature=1)
+
+def complete_text(text, n_chars=50, temperature=1):
+    for _ in range(n_chars):
+        text += next_char(text, temperature)
+    return text
+
+
+tf.random.set_seed(42)
+
+print("Song")
+def complete_song(seed_text, num_lines=4, chars_per_line=50, temperature=0.2):
+    song = seed_text
+
+    for _ in range(num_lines):
+        for _ in range(chars_per_line):
+            song += next_char(song, temperature)
+        song += '\n'  # Add a line break after each line
+
+    return song
+
+tf.random.set_seed(42)
+
+song = complete_song("Once upon a time", num_lines=8, chars_per_line=80, temperature=0.5)
+print(song)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print("Elapsed time: ", elapsed_time, "seconds")
